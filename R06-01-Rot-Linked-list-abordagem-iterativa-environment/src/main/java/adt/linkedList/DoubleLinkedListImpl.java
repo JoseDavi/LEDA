2500f798419abbfe3ea4e87c605083fb18963260
JoseDavi <jose.davi.souza@ccc.ufcg.edu.br>
2018-10-02 00:26:42 -0300

    R6 SLL completo

--- R06-01-Rot-Linked-list-abordagem-iterativa-environment/src/main/java/adt/linkedList/DoubleLinkedListImpl.java
+++ R06-01-Rot-Linked-list-abordagem-iterativa-environment/src/main/java/adt/linkedList/DoubleLinkedListImpl.java
@@ -4,23 +4,73 @@ public class DoubleLinkedListImpl<T> extends SingleLinkedListImpl<T> implements
 		DoubleLinkedList<T> {
 
 	protected DoubleLinkedListNode<T> last;
+	
+	public DoubleLinkedListImpl() {
+		head = new DoubleLinkedListNode<T>();
+		last = new DoubleLinkedListNode<T>();
+	}
 
 	@Override
 	public void insertFirst(T element) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		DoubleLinkedListNode<T> aux = (DoubleLinkedListNode<T>) head;
+		DoubleLinkedListNode<T> newNode = new DoubleLinkedListNode<T>(element, aux.previous, aux.previous);
+		if (head.isNIL()) {
+			last = newNode;
+		}
+		head = newNode;
 	}
 
 	@Override
 	public void removeFirst() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		if (!head.isNIL()) {
+			head = head.next;
+			DoubleLinkedListNode<T> aux = (DoubleLinkedListNode<T>) head;
+			aux.previous = new DoubleLinkedListNode<>();
+			head = aux;
+			if (head.isNIL()) {
+				last = (DoubleLinkedListNode<T>) head;
+			}
+		}
 	}
 
 	@Override
 	public void removeLast() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		if (!last.isNIL()) {
+			last = last.previous;
+			if (last.isNIL()) {
+				head = last;
+			}
+			last.next = new DoubleLinkedListNode<T>();
+		}
+	}
+	
+	public T search(T element) {
+		DoubleLinkedListNode<T> auxHead = (DoubleLinkedListNode<T>) head;
+		DoubleLinkedListNode<T> auxLast = last;
+		while (auxHead != auxLast && (DoubleLinkedListNode<T>)auxHead.next != auxLast && auxHead.data != element && auxLast.data != element) {
+			auxHead = (DoubleLinkedListNode<T>) auxHead.next;
+			auxLast = auxLast.previous;
+		}
+		if (auxHead.data == element) {
+			return auxHead.getData();
+		}else if (auxLast.data == element) {
+			return auxLast.getData();
+		}else {
+			return null;
+		}
+	}
+	
+	public void insert(T element) {
+		DoubleLinkedListNode<T> aux = last;
+		DoubleLinkedListNode<T> newNode = new DoubleLinkedListNode<T>(element, (DoubleLinkedListNode<T>) aux.next, aux.previous);
+		if (last.isNIL()) {
+			head = newNode;
+		}
+		last = newNode;
+	}
+	
+	public void remove(T element) {
+		
 	}
 
 	public DoubleLinkedListNode<T> getLast() {
--- R06-01-Rot-Linked-list-abordagem-iterativa-environment/src/main/java/adt/linkedList/SingleLinkedListImpl.java
+++ R06-01-Rot-Linked-list-abordagem-iterativa-environment/src/main/java/adt/linkedList/SingleLinkedListImpl.java
@@ -10,38 +10,83 @@ public class SingleLinkedListImpl<T> implements LinkedList<T> {
 
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented!");
+		if (head.isNIL()) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented!");
+		int size = 0;
+		SingleLinkedListNode<T> aux = head;
+		while (!aux.isNIL()) {
+			size++;
+			aux = aux.next;
+		}
+		return size;
 	}
 
 	@Override
 	public T search(T element) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented!");
+		T result = null;
+		SingleLinkedListNode<T> aux = head;
+		while (!aux.isNIL() && result == null) {
+			if (aux.data.equals(element)) {
+				result = aux.data;
+			}
+			aux = aux.next;
+		}
+		return result;
 	}
 
 	@Override
 	public void insert(T element) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented!");
+		SingleLinkedListNode<T> aux = head;
+		if (head.isNIL()) {
+			SingleLinkedListNode<T> newNode = new SingleLinkedListNode<T>(element, head);
+			head = newNode;
+		} else {
+			while (!aux.next.isNIL()) {
+				aux = aux.next;
+			}
+			SingleLinkedListNode<T> newNode = new SingleLinkedListNode<T>(element, aux.next);
+			aux.next = newNode;
+		}
 	}
 
 	@Override
 	public void remove(T element) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented!");
+		SingleLinkedListNode<T> previus = head;
+		SingleLinkedListNode<T> aux = head.next;
+		if (!head.isNIL()) {
+			if (head.getData().equals(element)) {
+				head = head.next;
+			} else {
+				while (!aux.isNIL() && !(aux.getData().equals(element))) {
+					previus = aux;
+					aux = aux.next;
+				}
+				if (!aux.isNIL()) {
+					previus.setNext(aux.next);
+				}
+			}
+		}
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public T[] toArray() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented!");
+		T[] result = (T[]) new Object[size()];
+		SingleLinkedListNode<T> aux = head;
+		int cont  = 0;
+		while (!aux.isNIL()) {
+			result[cont] = aux.getData();
+			aux = aux.next;
+			cont++;
+		}
+		return result;
 	}
 
 	public SingleLinkedListNode<T> getHead() {
